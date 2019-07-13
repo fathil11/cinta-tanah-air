27354f3b7c71528c2da362e95610bf022928a9fe
fathil11 <fathil.arham@gmail.com>
2019-07-14 03:31:15 +0700

    Add create_article_table Migration

---
+++ database/migrations/2019_07_13_202240_create_article_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->enum('type', ['berita', 'bertutur']);
+            $table->string('slug')->unique();
+            $table->string('banner_path');
+            $table->text('article', 5000);
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article');
+    }
+}
